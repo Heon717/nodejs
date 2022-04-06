56c79bc46e8684ee85d4ef3fbbc913668d8ea8d8
Heon717 <fiia2001@naver.com>
2022-04-06 16:55:18 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const app = express();                  // express 객체 생성
+const server = http.createServer(app);  // express http 서버 생성
+const io = socket(server);              // 생성된 서버를 socket.io 에 바인딩
+
+app.use('/css',express.static('./static/css'));
+app.use('/js',express.static('./static/js'));
+
+// get 방식으로 / 경로에 접속하면 실행 됨
+// request <-- 클라이언트에서 전달된 데이터와 정보들이 담겨있음
+// response <-- 클라리언트에게 응답할 정보가 담겨있음
+app.get('/',(request,response) => {
+    // console.log('유저가 / 으로 접속하였습니다 !');  // 터미널,서버에 표시
+    // response.send('Hello, 익스프레스 서버 !');  // 클라이언트에게 전달할 데이터
+
+    // readFile() -> 지정된 파일을 읽어서 데이터를 가져옴
+    fs.readFile('./static/index.html',(err,data) => {
+        if(err) {
+            response.send('에러');
+        } else {
+            // writeHead : 클라이언트에게 보낼 내용은 index.html 이기때문에 헤더에 설정
+            response.writeHead(200, {'Content-Type':'text/html'});
+            // write : HTML 데이터 전송
+            response.write(data);
+            // end : 완료 (write를 통해 응답할 경우 꼭 사용해야함!)
+            response.end();
+        }
+    })
+})
+
+// node app.js 로 실행 (명령어)
+server.listen(8080, () => {
+    console.log('서버 실행중 ...');
+});
